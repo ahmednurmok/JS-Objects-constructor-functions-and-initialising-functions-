6326c7d64306d00883a89bc3bf9b19dca7e3262c
ahmednurmok <ahmednurmok@gmail.com>
2025-11-20 22:34:40 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ function HouseKeeping  (Name, DOB, Ethnicity) {
 }
 var housekeeping2 = HouseKeeping('Ahmednur', '1/01/1900', 'Somali' ) 
 console.log(housekeeping2.DOB)
+
+// Constructor function with prpoperty and method
+
+function Housekeeper (yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.cleaning= function() {
+        console.log{'cleaningInProgress'}
+    }
+}
+
+var housekeeping3 = HouseKeeper('3', 'Ahmednur', ['bedroom', 'kitchen'] ) 
+housekeeping3.clean();
+// above will return the method as it is now saved in the constructor function.
+
